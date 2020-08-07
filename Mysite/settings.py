c07b60af825d153e95e5be2975492c0958db021c
Shikhar-Khare <69240246+Shikhar-Khare@users.noreply.github.com>
2020-08-07 13:35:52 +0530

    made changes to settings.py and urls.py files

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'root')
+
+DATA_URL = '/data/'
+DATA_ROOT = os.path.join(BASE_DIR,'data')
+
+
+
